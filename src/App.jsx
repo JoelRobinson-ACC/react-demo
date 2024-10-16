129dc55bf5043e5d44a6d9ff3c4c0c362e40c172
JoelRobinson-ACC <143213508+JoelRobinson-ACC@users.noreply.github.com>
2024-10-16 11:53:34 +0100

    Added wiggle anim

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,14 @@ const App = () => {
       <NavBar />
       <div className="content p-2">
 
+        {/* <div className="flex gap-8 justify-center p-8">
+          <div className="bg-blue-500 w-1/6 aspect-square animate-spin-slow"></div>
+          <div className="bg-red-500 w-1/6 aspect-square animate-ping-easy">
+          </div>
+          <div className="bg-green-500 w-1/6 aspect-square animate-pulse"></div>
+          <div className="bg-yellow-500 w-1/6 aspect-square hover:animate-wiggle"></div>
+        </div> */}
+
         {isPending && <p>Loading...</p>}
 
         {data && data.posts.map((p) => (
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 3s linear',
+        'ping-easy': 'ping 3s ease-in infinite',
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      }
+    },
   },
   plugins: [],
 }
